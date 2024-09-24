d7152d778fb7ed3d7818e7710b07b8e802ff5124
Yuval <117393676+TheSevenCs@users.noreply.github.com>
2024-09-23 20:02:18 -0400

    Main function now parses the data and puts it into a vector space

--- ASDT A2/Source.cpp
+++ ASDT A2/Source.cpp
@@ -1,3 +1,48 @@
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <vector>
+#include <string>
+
+struct STUDENT_DATA {
+    std::string firstName;
+    std::string lastName;
+
+    STUDENT_DATA(const std::string& first, const std::string& last)
+        : firstName(first), lastName(last) {}
+};
+
 int main() {
-	return 1;
+    std::string filename = "StudentData.txt";
+    std::vector<STUDENT_DATA> students;
+    std::ifstream inputFile(filename);
+    std::string line;
+
+    if (!inputFile.is_open()) {
+        std::cerr << "Error opening file: " << filename << std::endl;
+        return 1;
+    }
+
+    while (std::getline(inputFile, line)) {
+        std::istringstream ss(line);
+        std::string firstName, lastName;
+
+        if (std::getline(ss, firstName, ',') && std::getline(ss, lastName)) {
+            firstName.erase(0, firstName.find_first_not_of(" \n\r\t"));
+            firstName.erase(firstName.find_last_not_of(" \n\r\t") + 1);
+            lastName.erase(0, lastName.find_first_not_of(" \n\r\t"));
+            lastName.erase(lastName.find_last_not_of(" \n\r\t") + 1);
+
+            students.emplace_back(firstName, lastName);
+        }
+    }
+
+    inputFile.close();
+
+    for (std::size_t i = 0; i < students.size(); ++i) {
+        std::cout << "First Name: " << students[i].firstName
+            << ", Last Name: " << students[i].lastName << std::endl;
+    }
+
+    return 0;
 }
